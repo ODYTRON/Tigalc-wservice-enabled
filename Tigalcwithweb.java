4910ffae8f380c21367923c0a100f1ff457d08c4
Odysseas Tigas <OTIGAS@HOTMAIL.COM>
2018-04-21 21:06:26 +0300

    Create Tigalcwithweb.java

---
+++ Tigalcwithweb.java
@@ -0,0 +1,8 @@
+public class Tigalcwithweb {
+
+    public static void main(String[] args) {
+  
+        System.out.println("This android simple calculator with web services is written in Java");
+    }
+
+}
